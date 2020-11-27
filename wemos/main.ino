da4f5ac5ebec168280569099525431aafbd6c249
DutchEllie <rotzooi12@outlook.com>
2020-11-27 16:06:47 +0100

    set up wemos vscode

---
+++ wemos/main.ino
@@ -0,0 +1,15 @@
+#include <ESP8266WiFi.h>
+// Network SSID
+const char* ssid = "Tyfus";
+const char* password = "Cholera";
+ 
+void setup() {
+
+    WiFi.softAP("ESPsoftAP_Anus", "pass-to-soft-AP");
+ 
+}
+ 
+void loop() {
+  
+//Add your project's loop code here
+  }
