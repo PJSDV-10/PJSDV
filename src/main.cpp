af40e2b74f1c74cc99c18f4fc29805735d2dab95
DutchEllie <quenten.s@outlook.com>
2020-12-07 08:46:51 +0000

    comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "websocket.h"
 
 int main(){
+    /* There is quite enough comments in the websocket.cpp file. The main just creates a SocketServer class on port 8080, accepts the first incoming request and prints the message the remote sends. Then it sends the msg and closes the connection. */
     SocketServer sv(8080);
     sv.ListenAndAccept();
     sv.printMessage();
--- src/websocket.cpp
+++ src/websocket.cpp
@@ -20,40 +20,49 @@ SocketServer::SocketServer(int port)
     }
 
     fillInHints();
+
+    //Convert the int port into a const char* (which has to be more difficult than it should be of course...)
     std::string ports = std::to_string(port);
     const char *portch = ports.c_str();
 
+    //We retrieve the address information for the socket
     if ((status = getaddrinfo(NULL, portch, &hints, &serverInfo)) != 0)
     {
         fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
         exit(1);
     }
-    int yes = 1;
 
+    /*This is a little setting we need to change. If we don't set SO_REUSEADDR to true then the program during bind() might get blocked because the port might already be bound. This can happen if we run this program a couple times in succession.*/
+    int yes = 1;
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes))
     {
         perror("Failed setting \"Reuse Address\" option for socket");
         exit(EXIT_FAILURE);
     }
 
+    /* Bind the socket server_fd to the port we specified earlier with getaddrinfo(). Finally the socket is bound and can be used. */
     if (bind(server_fd, serverInfo->ai_addr, serverInfo->ai_addrlen))
     {
         perror("Binding to port failed");
         exit(EXIT_FAILURE);
     }
 
-
+    /* The serverInfo is not needed anymore, as the socket is already bound at this point. This essentially just frees all the struct. It's a specialized function because serverInfo is a addrinfo struct, which is a linked list. It's made specially for this struct and is a native function. */
     freeaddrinfo(serverInfo);
 }
 
-
+/* Start listening on the server_fd socket and accept any incoming connections. */
 void SocketServer::ListenAndAccept()
 {
+    /* Start listening on server_fd. This is essentially a setting and therefor non-blocking. The 20 in there is the limit of how many concurrent requests there can be. Unfortunately at this point there is no forking, nor any multithreading yet. */
     if (listen(server_fd, 20))
     {
         perror("Listening failed");
         exit(EXIT_FAILURE);
     }
+
+    /* Set the addr_size to the size of the their_addr sockaddr struct. This is a little off from how it's supposed to be though. The their_addr struct is not supposed to be a sockaddr but a sockaddr_storage, but I couldn't get this to work. 
+    The accept() function is blocking here and waits for an incoming signal. It then creates a new socket called remote_fd. This is a new socket that can be used. The old server_fd is still listening. Normally this would be put into an accept loop and the processes would then be handled by forks or other threads. */
     addr_size = sizeof(their_addr);
     remote_fd = accept(server_fd, &their_addr, &addr_size);
 
@@ -64,6 +73,7 @@ void SocketServer::ListenAndAccept()
     }
 }
 
+/* Prints any incoming message to the terminal */
 void SocketServer::printMessage()
 {
     char buffer[1024] = {0};
@@ -72,6 +82,7 @@ void SocketServer::printMessage()
               << buffer << std::endl;
 }
 
+/* Sends a message to the remote host. */
 void SocketServer::sendMessage(const char *msg)
 {
     int len = strlen(msg);
@@ -79,6 +90,7 @@ void SocketServer::sendMessage(const char *msg)
     send(remote_fd, &msg, len, flags);
 }
 
+/* Closes the sockets. This will be made cleaner later once I figure out how it works exactly. */
 void SocketServer::closeAll()
 {
     if (close(remote_fd))
@@ -93,6 +105,11 @@ void SocketServer::closeAll()
     }
 }
 
+/* fillInHints() sets the static settings for the later to be used getAddrInfo() function.
+Here we populate a addrinfo struct with zeros and then set the following settings:
+    ai_family = AF_UNSPEC (IPv4 and IPv6 are fine)
+    ai_socktype = SOCK_STREAM (TCP stream socket)
+    ai_flags = AI_PASSIVE (automatically choose my IP)*/
 void SocketServer::fillInHints()
 {
     memset(&hints, 0, sizeof hints); // make sure the struct is empty
