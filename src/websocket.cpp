99d901fb32c658b8d28abcb7d7a73cff6d1f6d2c
DutchEllie <quenten.s@outlook.com>
2020-12-07 11:03:50 +0000

    commit

--- src/websocket.cpp
+++ src/websocket.cpp
@@ -78,8 +78,9 @@ void SocketServer::printMessage()
 {
     char buffer[1024] = {0};
     recv(remote_fd, buffer, 1024, 0);
-    std::cout << "Data read:\n"
-              << buffer << std::endl;
+    //std::cout << "Data read:\n"
+    //          << buffer << std::endl;
+    printf("%s\n",buffer ); 
 }
 
 /* Sends a message to the remote host. */
