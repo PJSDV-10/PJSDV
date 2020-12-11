1557faa4809f425aed214807fb21c78fc0ecab86
Lars van der Wijden <sral8991@gmail.com>
2020-12-11 15:43:46 +0100

    test10

--- html/loginForm.php
+++ html/loginForm.php
@@ -6,16 +6,16 @@
 <div class="main">
 	<form action="login.php" method="post">
 		<label>Username:</label></br>
-		<input type="text" name="username"></br>
+		<input class="textbox" type="text" name="username"></br>
 		<label>Password:</label></br>
-		<input type="password" name="password"></br>
+		<input class="textbox" type="password" name="password"></br>
 		<?php
 			if(isset($_GET['err'])) {
 				echo "<label>" . $_GET['err'] . "</label>";
 			}
 		?>
 		</br>
-		<input type="submit" value="submit">
+		<input class="button" type="submit" value="submit">
 	</form>
 </div>
 
