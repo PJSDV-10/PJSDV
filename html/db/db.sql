61142bcf04e8da2a72d353e2197a6fde623060d4
Lars van der Wijden <sral8991@gmail.com>
2021-01-15 14:39:19 +0100

    update db to full db

--- html/db/db.sql
+++ html/db/db.sql
@@ -8,3 +8,17 @@ CREATE TABLE users (
 	PRIMARY KEY (usr_ID),
 	UNIQUE (usr_ID, username)
 );
+
+CREATE TABLE rooms (
+	room_number int NOT NULL,
+	types int NOT NULL,
+	PRIMARY KEY (room_number),
+	UNIQUE (room_number)
+);
+
+CREATE TABLE devices (
+	type_number int NOT NULL,
+	device_name varchar(255) NOT NULL,
+	PRIMARY KEY (type_number),
+	UNIQUE (type_number)
+);
