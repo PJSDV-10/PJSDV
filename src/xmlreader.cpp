7f9ee8d9eaf4fb01a051c8fd055e7ee2f15919a8
DutchEllie <quenten.s@outlook.com>
2021-01-15 15:44:28 +0000

    recovered try catch for doc parsing

--- src/xmlreader.cpp
+++ src/xmlreader.cpp
@@ -60,12 +60,12 @@ XmlReader::XmlReader(const char *xmldoc)
 
     char xml[4096] = {};
     strcpy(xml, xmldoc);
-    //try{
+    try{
         doc->parse<0>(xml);
-    //}catch(parse_error){
-    //    std::cout << "A parsing error occured. Document is not valid XML" << std::endl;
-    //    return;
-    //}
+    }catch(parse_error){
+        std::cout << "A parsing error occured. Document is not valid XML" << std::endl;
+        return;
+    }
     xml_node<> *root_node = doc->first_node("message");
     this->root_node = root_node;
 
