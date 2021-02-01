d8f915b935954aa39dd3a71303cb703403ec5917
Lupindakaas <57939116+Lupindakaas@users.noreply.github.com>
2021-02-01 12:20:13 +0100

    receive function optimalised
    first it peeks if there is something to read before trying to read it.

--- wemos/stoel/main/Xml.ino
+++ wemos/stoel/main/Xml.ino
@@ -26,9 +26,10 @@ std::string buildStatusMsg(std::string function){
           worth = "1";
         } else
           worth = "0";
+          knopAan = 0;
        }
        
-       if (sensorNames[i][1].compare("pushButton") == 0 && sensor[1][0] == 1) {
+       if (sensorNames[i][1].compare("pushButton") == 0 && sensor[i][0] == 1) {
         if (knopAan) {
           worth = "0";
           knopAan = 0;
