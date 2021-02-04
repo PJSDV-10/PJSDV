d0f1f4a8211cf42d3d08532c38fc108da05ff22c
Ernest Liebeton <E.liebeton@gntel.nl>
2021-02-04 18:33:24 +0100

    Fix for reader?

--- src/wemos.h
+++ src/wemos.h
@@ -25,7 +25,7 @@ public:
     //virtual const bool getStatus(std::string sensor) = 0;
     virtual std::string handleSensorUpdate(XmlReader *) = 0;
     virtual std::string handleWebsiteUpdate(XmlReader *) = 0;
-    std::string website(XmlReader *,int) = 0;
+    virtual std::string website(XmlReader *,int) = 0;
 
     //virtual XmlReader* sendStatusRequest();
     //    virtual std::string handleSiteRequest();
